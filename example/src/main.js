2533db10440f8f7901fe8ed360d0721ea0032e09
traducer <traducer21@gmail.com>
2016-06-11 05:59:11 +0000

    socket-clien.js needs a semi-colon at the end of the iife, updated the example project

--- example/src/main.js
+++ example/src/main.js
@@ -1,10 +1,10 @@
 const one = require('./levelone/one')
 const square = document.createElement('div')
 
-console.log(one.hello('hello there'))
+console.log(one.hello('hello sir'))
 
 square.style.width = '200px'
-square.style.height = '200px'
+square.style.height = '20px'
 square.style.background = 'red'
 
 document.body.appendChild(square)
