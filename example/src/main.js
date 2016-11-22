1b5c160526036a7de46af4ebcfba306ed5bed02f
traducer <traducer21@gmail.com>
2016-11-22 12:16:22 +0000

    updated example

--- example/src/main.js
+++ example/src/main.js
@@ -1,8 +1,6 @@
 const one = require('./levelone/one')
 const square = document.createElement('div')
 
-console.log(one.hello('hello sir'))
-
 square.style.width = '200px'
 square.style.height = '20px'
 square.style.background = 'red'
